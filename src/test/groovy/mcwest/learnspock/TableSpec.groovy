f81d58ba68125d5d8c2e388b84b7160a560f6c0b
Brian Westrich <bw@mcwest.com>
2015-02-06 17:11:47 -0600

    add simple table example

---
+++ src/test/groovy/mcwest/learnspock/TableSpec.groovy
@@ -0,0 +1,18 @@
+package mcwest.learnspock
+
+import spock.lang.Specification
+
+class TableSpec extends Specification {
+    def "length of Spock's and his friends' names"() {
+        expect:
+        name.size() == length
+
+        where:
+        name     | length
+        "Spock"  | 5
+        "Kirk"   | 4
+        "Scotty" | 6
+    }
+}
+
+
